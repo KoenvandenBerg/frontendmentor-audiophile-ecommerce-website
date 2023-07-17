081127a0812e777f5bc8ce82322b6e0f19859b00
Koen van den Berg <git@koenvandenberg.dev>
2023-07-17 19:03:26 +0200

    Restructure Header to allow for header info text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Button, { ButtonTypes } from './components/Button';
 import pageStyles from './page.module.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Button text="test" type={ButtonTypes.default} onClick={() => {}} />
+      <Button text="test" type={ButtonTypes.outline} onClick={() => {}} />
+      <Button text="test" type={ButtonTypes.transparent} onClick={() => {}} />
+    </div>
+  );
 }
