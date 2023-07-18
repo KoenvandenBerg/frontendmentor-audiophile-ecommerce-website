f16abc9ec7b08aa0bfbf50699ad543d97aba7555
Koen van den Berg <git@koenvandenberg.dev>
2023-07-18 15:03:34 +0200

    Add and style footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import Button, { ButtonTypes } from './components/Button';
 import pageStyles from './page.module.css';
 
 export default function Home() {
-  return (
-    <div>
-      Home
-      <Button text="test" type={ButtonTypes.default} onClick={() => {}} />
-      <Button text="test" type={ButtonTypes.outline} onClick={() => {}} />
-      <Button text="test" type={ButtonTypes.transparent} onClick={() => {}} />
-    </div>
-  );
+  return <div className={pageStyles.temporary}>Home</div>;
 }
