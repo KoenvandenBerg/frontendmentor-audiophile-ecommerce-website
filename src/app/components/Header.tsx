bf55a338a6900ec76b62b2993c73eede0b707f22
Koen van den Berg <git@koenvandenberg.dev>
2023-07-18 11:29:31 +0200

    Add and style hero text

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import headerStyles from '@/app/styles/Header.module.css';
 import Link from 'next/link';
 import HamburgerMenu from './HamburgerMenu';
+import Button, { ButtonTypes } from './Button';
 
 const pages = ['Home', 'Headphones', 'Speakers', 'Earphones'];
 
@@ -10,7 +11,9 @@ export default function Header() {
     <div className={headerStyles.headerContainer}>
       <div className={headerStyles.headerContentContainer}>
         <nav className={headerStyles.headerNavContainer}>
-          <HamburgerMenu />
+          <div className={headerStyles.headerHamburgerMenu}>
+            <HamburgerMenu />
+          </div>
           <svg
             width="143"
             height="25"
@@ -58,7 +61,21 @@ export default function Header() {
             </svg>
           </button>
         </nav>
-        <div className={headerStyles.heroText}></div>
+        <div className={headerStyles.heroTextContainer}>
+          <div className={headerStyles.heroText}>
+            <h3>New Product</h3>
+            <h2>XX99 Mark II HeadphoneS</h2>
+            <p>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <Button
+              text="See Product"
+              type={ButtonTypes.default}
+              onClick={() => {}}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
