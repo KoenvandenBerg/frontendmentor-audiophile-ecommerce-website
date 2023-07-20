e4142d5aba801eb3fc2b0f3d3f3f08deb260a61c
Koen van den Berg <git@koenvandenberg.dev>
2023-07-20 23:47:35 +0200

    Add and style CategoryHeader component

---
+++ src/app/components/CategoryHeader.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import categoryHeaderStyles from '@/app/styles/CategoryHeader.module.css';
+import Navbar from './Navbar';
+import { usePathname } from 'next/navigation';
+
+type CategoryHeaderProps = {
+  categoryName: string;
+};
+
+export default function CategoryHeader(props: CategoryHeaderProps) {
+  return (
+    <div className={categoryHeaderStyles.container}>
+      <div className={categoryHeaderStyles.contentContainer}>
+        <div className={categoryHeaderStyles.navbarContainer}>
+          <Navbar />
+        </div>
+        <h1>{props.categoryName}</h1>
+      </div>
+    </div>
+  );
+}
