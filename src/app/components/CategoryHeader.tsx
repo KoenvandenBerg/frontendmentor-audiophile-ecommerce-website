c9862670ceb10d56b064f673a2a9280d47de7966
Koen van den Berg <git@koenvandenberg.dev>
2023-07-20 23:55:12 +0200

    Remove unused import from ProductHeader

--- src/app/components/CategoryHeader.tsx
+++ src/app/components/CategoryHeader.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import categoryHeaderStyles from '@/app/styles/CategoryHeader.module.css';
 import Navbar from './Navbar';
-import { usePathname } from 'next/navigation';
 
 type CategoryHeaderProps = {
   categoryName: string;
